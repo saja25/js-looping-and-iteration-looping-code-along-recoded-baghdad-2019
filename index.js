c045805b1438fa563f518d0bb5bed32a9012ab4f
saja amer <sajaamer86@gmail.com>
2019-09-22 17:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let arr=["Ada", "Brendan", "Ali"];
+let eve="birthday";
+function writeCards(arr,eve){
+   var thank=[];
+ for (let i=0;i<arr.length;i++){
+ thank[i]="Thank you, "+arr[i]+", for the wonderful "+eve+" gift!"
+ }
+return thank ;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+//-------------
+function countdown(num){
+ while (num>=0){
+  console.log(num--);
+ }
+ return num;
+}
+countdown(10);
